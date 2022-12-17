4d3516661975371ab8e1e93d3e10f56b6d912d7c
unknown <lopez.thinktank@gmail.com>
2022-12-17 15:50:52 -0800

    updated more of the my file structure and deleted some files not being used. Started with some new components files.

---
+++ john_lopez_portfolio/src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { ReactDOM } from "react";
+// import * as servicWorker from '.serviceWorker';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
